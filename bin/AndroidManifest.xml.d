a6a5974c13a986b29ed748c6d13f81ebde90a12e
Bartold <batroxapps@hotmail.com>
2014-06-09 19:08:17 +0200

    deleted bin and gen

--- bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-D:\VOORBACKUP\Documents\programmeren\android\The Great Gun\bin\AndroidManifest.xml : \
-D:\VOORBACKUP\Documents\programmeren\android\The Great Gun\AndroidManifest.xml \
--- bin/TheGreatGun-debug-unaligned.apk.d
+++
@@ -1,3 +0,0 @@
-D:\VOORBACKUP\Documents\programmeren\android\The Great Gun\bin\TheGreatGun-debug-unaligned.apk : \
-D:\VOORBACKUP\Documents\programmeren\android\The Great Gun\bin\TheGreatGun.ap_ \
-D:\VOORBACKUP\Documents\programmeren\android\The Great Gun\bin\classes.dex \
